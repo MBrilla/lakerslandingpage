7905348fa4a03b86c6cc63657d45b535a4218151
MBrilla <mjblebrilla@gmail.com>
2025-03-23 21:14:07 +0800

    Update repository with latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lakerslandingpage/' // Set the base path to match your GitHub Pages URL
+  base: '/lakerslandingpage/' // Ensure this matches your repository name
 })
